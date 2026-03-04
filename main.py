c8903fc1796cb728a98fd1980f60a2b6018c337f
UdayHelps <udaykatare1@gmail.com>
2026-03-04 15:38:14 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+import requests
+from prediction import predict_routes
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status":"EscapeRoute AI running"}
+
+@app.get("/escape_routes")
+def escape_routes(origin:str):
+
+    url="https://opensky-network.org/api/states/all"
+
+    r=requests.get(url)
+    data=r.json()
+
+    states=data["states"]
+
+    routes={}
+
+    for s in states:
+
+        callsign=s[1]
+
+        if not callsign:
+            continue
+
+        airline=callsign[:3]
+
+        dest="UNKNOWN"
+
+        if dest not in routes:
+            routes[dest]=0
+
+        routes[dest]+=1
+
+    results=[]
+
+    for r in routes:
+
+        results.append({
+            "destination":r,
+            "active_flights":routes[r]
+        })
+
+    results.sort(key=lambda x:x["active_flights"], reverse=True)
+
+    return results[:10]
